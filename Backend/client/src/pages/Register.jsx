40ddbd4e1f7dc3b1f85022a7ab64d3b9abc9274c
anoop <anoopaathreya@gmail.com>
2024-03-12 18:10:46 +0530

    fix

--- Backend/client/src/pages/Register.jsx
+++ Backend/client/src/pages/Register.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -23,8 +24,11 @@ function Register() {
       }
     );
 
-    // const data = await response.json();
+    const data = await response.json();
     // console.log(data);
+    if (response.ok) {
+      navigate("/login");
+    }
   };
 
   return (
